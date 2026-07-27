d0fd08e9a241c1b59b3771c51fbff5e5da915011
Luci <41099231+kido-luci@users.noreply.github.com>
2026-07-27 17:06:31 +0700

    add 1004

---
+++ lib/leetcode/1004. Max Consecutive Ones III/solution_1.dart
@@ -0,0 +1,34 @@
+// Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+// Output: 10
+// Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+// Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+// [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
+// [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+//      1,1,1,1,1,1,1,1,1,1, len = result = 10
+//      1,1,0,0,1,1,1,0,1,1, total 0 = 3 = k
+import 'dart:math';
+
+// ~402ms
+// stacks0.removeAt(0) => 0n^2
+class Solution {
+  int longestOnes(List<int> nums, int k) {
+    final stacks0 = <int>[];
+    var longest = 0;
+    int last0Index = -1;
+
+    for (var i = 0; i < nums.length; i++) {
+      if (nums[i] == 0) {
+        stacks0.add(i);
+      }
+
+      if (stacks0.length > k) {
+        last0Index = stacks0.removeAt(0);
+      }
+
+      longest = max(longest, i - last0Index);
+    }
+
+    return longest;
+  }
+}
